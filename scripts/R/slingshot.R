fb2c7dab2812d7901f0af15df73e8a970c275d28
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-04-09 15:28:35 +0200

    Update clustering after components search + slingshot

--- scripts/R/slingshot.R
+++ scripts/R/slingshot.R
@@ -1,3 +1,24 @@
 # packages
 install.packages("BiocManager")
 install.packages("slingshot")
+install.packages("devtools")
+BiocManager::install("SingleCellExperiment")
+devtools::install_github("wesm/feather/R")
+
+# libraries
+library(slingshot, quietly = TRUE)
+library(feather, quietly = TRUE)
+library(SingleCellExperiment, quietly = TRUE)
+
+df <- read_feather("./notebooks/tmp/data.feather")
+
+exp <- SingleCellExperiment(list(fake=matrix(seq(1, nrow(df)), nrow=1)))
+reducedDims(exp) <- list(PCA = data.matrix(df[grep("^feat_pca", names(df))]))
+colData(exp)$BGMM <- df$meta_cluster_label
+
+res <- slingshot(
+  data = exp,
+  clusterLabels = "BGMM",
+  reducedDim = "PCA",
+  approx_points = 100
+)
