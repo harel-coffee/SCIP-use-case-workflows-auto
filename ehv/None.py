4c4ac7c8809511cd89777f05b04bc256b5d7c91b
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-11-09 17:32:28 +0100

    Add comparison with IDEAS features

--- ehv/None.py
+++ ehv/None.py
@@ -99,3 +99,53 @@ from ehv import load as e_load, core
 plt.rcParams['figure.facecolor'] = 'white'
 
 numpy.random.seed(42)
+
+# Cell
+# export
+
+import pandas
+import os
+import numpy
+import seaborn
+import logging
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib
+from importlib import reload
+from ehv import core
+from joblib import load, dump
+from pathlib import Path
+import uuid
+import re
+import scipy
+
+from ehv import load as e_load, core
+
+plt.rcParams['figure.facecolor'] = 'white'
+
+numpy.random.seed(42)
+
+# Cell
+# export
+
+import pandas
+import os
+import numpy
+import seaborn
+import logging
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib
+from importlib import reload
+from ehv import core
+from joblib import load, dump
+from pathlib import Path
+import uuid
+import re
+import scipy
+
+from ehv import load as e_load, core
+
+plt.rcParams['figure.facecolor'] = 'white'
+
+numpy.random.seed(42)
--- ehv/_nbdev.py
+++ ehv/_nbdev.py
@@ -19,11 +19,32 @@ index = {"load_config": "00_core.ipynb",
          "tag_columns": "1a_load.ipynb",
          "add_merged_col": "1a_load.ipynb",
          "get_logicle_xform": "1e_viral_infection_progression.ipynb",
-         "logicle_transform": "1e_viral_infection_progression.ipynb"}
+         "logicle_transform": "1e_viral_infection_progression.ipynb",
+         "Correlation": "2c_selected_features_correlation_analysis.ipynb",
+         "GroupedTransformer": "3c_preprocessing_pipeline_low.ipynb",
+         "ColumnTransformer": "3c_preprocessing_pipeline_low.ipynb",
+         "PandasVarianceThreshold": "3c_preprocessing_pipeline_low.ipynb",
+         "make_pipeline_1": "3c_preprocessing_pipeline_ctrl.ipynb",
+         "define_dummy": "4c_optimal_mrna_content_regression.ipynb",
+         "define_mlp": "4c_optimal_mrna_content_regression.ipynb",
+         "define_xgb": "4c_optimal_mrna_content_regression.ipynb",
+         "define_sgd": "4c_optimal_mrna_content_regression.ipynb",
+         "definition_dict": "4c_optimal_mrna_content_regression.ipynb",
+         "objective": "4c_optimal_mrna_content_regression.ipynb",
+         "run_optimization": "4c_optimal_mrna_content_regression.ipynb",
+         "nested_cross_validation_with_hyperparam_optimization": "4c_optimal_mrna_content_regression.ipynb",
+         "nested_cross_validation_with_hyperparam_optimization_command": "4c_optimal_mrna_content_regression.ipynb",
+         "func": "4i_GMM_clustering_all_COI.ipynb",
+         "n_components_search": "4i_GMM_clustering_all_COI.ipynb"}
 
 modules = ["core.py",
            "load.py",
-           "viral.py"]
+           "viral.py",
+           "fluorescence_intensity.py",
+           "correlation.py",
+           "preprocessing_pipeline.py",
+           "mrna_content_regression.py",
+           "gmm_clustering.py"]
 
 doc_url = "https://mlippeve.github.io/ehv/"
 
