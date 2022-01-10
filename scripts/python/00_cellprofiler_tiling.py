1358552f919fcce56f66fdd94b9ce913776709e2
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-01-10 20:30:25 +0100

    Update tiling and dependencies

--- scripts/python/00_cellprofiler_tiling.py
+++ scripts/python/00_cellprofiler_tiling.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import operator
 import pandas
 import click
+import zarr
 
 
 # In[32]:
@@ -30,7 +31,7 @@ def cropND(img, bounding):
     # compute possibly necessary padding widths
     padding = tuple(map(lambda a,b: abs(min(0, b-a)), bounding, img.shape))
     if sum(padding) > 0:
-      
+
         # split padding into before and after part
         before_after = tuple(map(lambda a: (a//2, (a//2)+(a%2)), padding))
         img = numpy.pad(
@@ -38,7 +39,7 @@ def cropND(img, bounding):
             pad_width=before_after,
             mode=padding_func
         )
-        
+
     start = tuple(map(lambda a, da: a//2-da//2, img.shape, bounding))
     end = tuple(map(operator.add, start, bounding))
     slices = tuple(map(slice, start, end))
@@ -48,48 +49,45 @@ def cropND(img, bounding):
 @click.command()
 @click.argument("part", type=str)
 def main(part):
-    images = list(Path("/user/gent/420/vsc42015/vsc_data_vo/datasets/weizmann/EhV/v2/images/").glob(f"{part}/*Ch1.*.tif"))
+    path = Path(f"/user/gent/420/vsc42015/vsc_data_vo/datasets/weizmann/EhV/v2/images/{part}.zarr")
+    images = zarr.open(path)
     tile_output = Path("/user/gent/420/vsc42015/vsc_data_vo/datasets/weizmann/EhV/v2/tiles/")
 
     ncols = 10
     nrows = 20
     ntiles = (len(images) // (ncols*nrows)) + ((len(images) % (ncols*nrows)) != 0)
     channels = [1, 3, 7]
+    channel_indices = [0, 2, 6]
     bounding = len(channels), 48, 48
-    
+
     print(f"Writing {ntiles} tiles for {len(images)} images")
 
     for i in range(ntiles):
+        out_path = tile_output / part / f"{i}_Ch{channels[0]}.tiff"
+        if out_path.exists():
+            continue
+
         tile = numpy.zeros(shape=(bounding[0], bounding[1]*nrows, bounding[2]*ncols), dtype=float)
 
         start = i * (ncols * nrows)
-        end = (i + 1) * (ncols * nrows)
-        for j, image in enumerate(images[start:end]):
-            paths = [
-                image.parent / image.name.replace("Ch1", "Ch"+str(c))
-                for c in channels
-            ]
-          
-            missing = False
-            for p in paths:
-                if not p.exists():
-                    print(f"{str(p)} is missing")
-                    missing = True
-          
-            if not missing:
-                row = j // ncols
-                col = j % ncols
-                pixels = cropND(tifffile.imread(paths), bounding)
+        end = min(len(images), (i + 1) * (ncols * nrows))
+        for j, idx in enumerate(range(start, end)):
+            pixels = images[idx].reshape(images.attrs["shape"][idx])[channel_indices]
 
-                tile[
-                    :,
-                    row*bounding[1]:(row+1)*bounding[1],
-                    col*bounding[2]:(col+1)*bounding[2]
-                ] = pixels
+            row = j // ncols
+            col = j % ncols
+            pixels = cropND(pixels, bounding)
+
+            tile[
+                :,
+                row*bounding[1]:(row+1)*bounding[1],
+                col*bounding[2]:(col+1)*bounding[2]
+            ] = pixels
 
         (tile_output /  part).mkdir(exist_ok=True)
         for j, t in enumerate(tile):
-            tifffile.imwrite(str(tile_output / part / f"{i}_Ch{channels[j]}.tiff"), t, photometric="minisblack")
+            out_path = tile_output / part / f"{i}_Ch{channels[j]}.tiff"
+            tifffile.imwrite(str(out_path), t, photometric="minisblack")
 
 
 # In[ ]:
