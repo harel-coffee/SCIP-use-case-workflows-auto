a4917081a05f461e2e4ef1bddae360356014bdd1
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-03-25 19:50:02 +0000

    Use link selections

--- scripts/python/ehv_panel.py
+++ scripts/python/ehv_panel.py
@@ -1,20 +1,38 @@
+from os import link
 import pandas
 import numpy
 import panel
 import hvplot.pandas  # noqa
 import hvplot
-from holoviews.streams import Selection1D
 import holoviews
 import pyarrow.parquet as pq
 from pathlib import Path
 from pandas.api.types import CategoricalDtype
 import holoviews.operation.datashader as hd
-from joblib import dump, load
+from holoviews.selection import link_selections
+import datashader
 
 
 data_dir = Path("/vsc/datasets/weizmann/EhV/v2/results/scip/202202071958/")
 
+
+def load_fake():
+    n = 1000
+
+    df = pandas.DataFrame({
+        "index": numpy.arange(n),
+        "meta_label": numpy.random.choice([0,1,2,3], size=n),
+        "meta_type": numpy.random.choice([0,1], size=n),
+        "feat_area": numpy.random.normal(loc=0, scale=2, size=n),
+        "dim_1": numpy.random.normal(loc=-1, scale=1.5, size=n),
+        "dim_2": numpy.random.normal(loc=2, scale=1, size=n)
+    })
+
+    return df
+
+
 def load():
+    from joblib import dump, load
 
     if (data_dir / "dump.joblib").exists():
         return load(data_dir / "dump.joblib")
@@ -26,22 +44,22 @@ def load():
             categories=sorted(df["meta_group"].astype(int).unique()), ordered=True)
         df["meta_group"] = df["meta_group"].astype(int).astype(cat_type)
         df["meta_replicate"] = df["meta_replicate"].astype(int)
-        
+
         df = df.set_index(
             ["meta_type", "meta_object_number", "meta_replicate", "meta_suffix", "meta_group"])
-        
+
         df = df[numpy.load(data_dir / "columns.npy", allow_pickle=True)]
         df = df.loc[numpy.load(data_dir / "index.npy", allow_pickle=True)]
-        
+
         df = df[df["meta_label"] != "unknown"]
         df["meta_label"] = pandas.Categorical(
-            df["meta_label"], 
-            categories=["mcp-_psba+", "mcp+_psba+", "mcp+_psba-", "mcp-_psba-"], 
+            df["meta_label"],
+            categories=["mcp-_psba+", "mcp+_psba+", "mcp+_psba-", "mcp-_psba-"],
             ordered=True
         )
 
         sel1 = ~(
-            (df.index.get_level_values("meta_group").isin([6,7,8,9])) & 
+            (df.index.get_level_values("meta_group").isin([6,7,8,9])) &
             (df.index.get_level_values("meta_type") == "Ctrl")
         )
         df = df[sel1]
@@ -59,30 +77,24 @@ def scatter(value):
     points = holoviews.Points(df, kdims=["dim_1", "dim_2"])
     return points.opts(color=value)
 
-
-def hist(index, col="feat_sum_DAPI", by="meta_type"):
-    if len(index) == 0:
-        index = df.index
-    return df.loc[index].hvplot.hist(col, by=by)
-
-
 df = load()
+# df = load_fake()
 
 autocomplete = panel.widgets.AutocompleteInput(
     name='Scatter hue', options=df.columns.tolist(),
     placeholder='Start typing here', value="meta_label")
 
-scat = hd.rasterize(scatter()
-    # holoviews.DynamicMap(
-    #     scatter, streams=[autocomplete.param.value]
-    # )
-).opts(tools=["lasso_select"])
+scat = holoviews.DynamicMap(
+    scatter, streams=[autocomplete.param.value]
+)
+datashaded = hd.datashade(scat, aggregator=datashader.count())
+spreaded = hd.dynspread(datashaded, threshold=0.50, how='over')
 
-sel = Selection1D(source=scat)
+bar1 = df.hvplot.hist("meta_label")
+bar2 = df.hvplot.hist("meta_type")
 
-h = holoviews.DynamicMap(hist, streams=[sel])
+link = link_selections.instance()
 
 panel.Column(
-    panel.Row(autocomplete),
-    panel.Row(scat + h, width_policy="max"),
-width_policy="max", height_policy="max").servable()
+    panel.Row(panel.Column(autocomplete, link(spreaded)), panel.Column(link(bar1), link(bar2))),
+).servable()
