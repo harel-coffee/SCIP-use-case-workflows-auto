7cad592d4a8c6419b383dc7b446272270e4ac440
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-04-27 15:53:25 +0200

    Merge upstream

--- scripts/python/wbc_xgb_parameter_search.py
+++ scripts/python/wbc_xgb_parameter_search.py
@@ -1,42 +1,43 @@
 from pathlib import Path
-import os
 import pickle
 
 import numpy
-import pandas
 import pyarrow.parquet as pq
 
 from xgboost import XGBClassifier
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, HalvingRandomSearchCV
+from sklearn.experimental import enable_halving_search_cv
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.pipeline import make_pipeline
 
 
 # LOAD DATA
 
-df = pq.read_table("/home/maximl/scratch/data/wbc/results/scip/202201141724/features.parquet").to_pandas()
+data_dir = Path(
+    "/home/maximl/scratch/data/wbc/results/scip/"
+)
+
+df = pq.read_table(data_dir / "features.parquet").to_pandas()
+
 df["meta_group"] = df["meta_group"].astype(int)
 df["meta_part"] = df["meta_part"].astype(int)
-df = df[numpy.load("/home/maximl/scratch/data/wbc/results/scip/202201141724/columns.npy", allow_pickle=True)]
-index = numpy.load("/home/maximl/scratch/data/wbc/results/scip/202201141724/index.npy", allow_pickle=True)
+
+df = df.set_index(["meta_object_number", "meta_part", "meta_group", "meta_fix"])
+df = df[numpy.load(data_dir / "columns.npy", allow_pickle=True)]
+index = numpy.load(data_dir / "index.npy", allow_pickle=True)
 df = df.loc[index]
 
 df = df[~df["meta_bbox_minr"].isna()]
 df = df[df["meta_label"] != "unknown"]
 
-glcms = set(df.filter(regex=".*glcm.*(BF1|BF2|SSC)$").columns)
-combined_glcms = set(df.filter(regex=".*combined.*glcm.*(BF1|BF2|SSC)$").columns)
-noncomb_glcms = glcms - combined_glcms
-df = df.drop(columns=noncomb_glcms)
-df.shape
-
-df = df.set_index(["meta_object_number", "meta_part", "meta_group", "meta_fix"])
+df = df.fillna(0)
 
 # PREP CLASSIFICATION INPUT
 
-neuts = df[df["meta_label"] == "CD15 + Neutrophils"].sample(n=110000).index
-df = df.drop(labels=neuts)
+# neuts = df[df["meta_label"] == "CD15 + Neutrophils"].sample(n=110000).index
+# df = df.drop(labels=neuts)
 
 enc = LabelEncoder().fit(df["meta_label"])
 y = enc.transform(df["meta_label"])
@@ -46,38 +47,75 @@ Xs = df.filter(regex="(BF1|BF2|SSC)$")
 
 # SPLIT DATA
 
-Xs_train, Xs_test, y_train, y_test =  train_test_split(Xs, y, test_size=0.3, random_state=0)
+Xs_train, Xs_test, y_train, y_test =  train_test_split(
+    Xs, y, stratify=y, test_size=1/3, random_state=0)
 
 # PARAMETER SEARCH
 
 model = make_pipeline(
     RandomOverSampler(sampling_strategy="not majority", random_state=0),
-    XGBClassifier(
-        booster="gbtree",  
-        objective="multi:softmax", 
-        eval_metric="merror",
-        tree_method="gpu_hist",
-        use_label_encoder=False
+    RFE(
+        XGBClassifier(
+            booster="gbtree",
+            objective="multi:softmax",
+            eval_metric="merror",
+            tree_method="gpu_hist",
+            use_label_encoder=False
+        )
     )
 )
 
-scoring = ('balanced_accuracy', 'f1_macro', 'f1_micro', 'precision_macro', 'precision_micro', 'recall_macro', 'recall_micro')
-grid = RandomizedSearchCV(
-    model,
-    {
-        "xgbclassifier__max_depth": [6, 5, 4, 3, 2],
-        "xgbclassifier__learning_rate": [0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01],
-        "xgbclassifier__n_estimators": [100, 200, 400, 600, 800, 1000],
-        "xgbclassifier__subsample": [0.25, 0.5, 0.75, 1],
-        "xgbclassifier__colsample_bytree": [0.25, 0.5, 0.75, 1]
+# scoring = (
+#     'balanced_accuracy',
+#     'f1_macro', 'f1_micro',
+#     'precision_macro', 'precision_micro',
+#     'recall_macro', 'recall_micro'
+# )
+# grid = RandomizedSearchCV(
+#     model,
+#     {
+#         "rfe__estimator__max_depth": [6, 5, 4, 3, 2],
+#         "rfe__estimator__learning_rate": [0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01],
+#         "rfe__estimator__n_estimators": [100, 200, 400, 600, 800, 1000],
+#         "rfe__estimator__subsample": [0.25, 0.5, 0.75, 1],
+#         "rfe__estimator__colsample_bytree": [0.25, 0.5, 0.75, 1],
+#         "rfe__n_features_to_select": [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+#     },
+#     n_iter=1000,
+#     refit=False,
+#     n_jobs=30,
+#     cv=5,
+#     scoring=scoring,
+#     verbose=2,
+#     return_train_score=True
+# ).fit(
+#     Xs_train,
+#     y_train
+# )
+
+grid = HalvingRandomSearchCV(
+    estimator=model,
+    param_distributions={
+        "rfe__estimator__max_depth": [6, 5, 4, 3, 2],
+        "rfe__estimator__learning_rate": [0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05, 0.01],
+        "rfe__estimator__subsample": [0.25, 0.5, 0.75, 1],
+        "rfe__estimator__colsample_bytree": [0.25, 0.5, 0.75, 1],
+        "rfe__n_features_to_select": [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1],
+        "randomoversampler__sampling_strategy": ["not majority", dict()]
     },
-    n_iter=1000,
+    factor=3,
+    resource='n_estimators',
+    n_candidates=3000,
+    max_resources=4500,
+    min_resources=2,
+    aggressive_elimination=True,
     refit=False,
-    n_jobs=30,
-    cv=3,
-    scoring=scoring,
-    verbose=2,
-    return_train_score=True
+    n_jobs=8,
+    cv=5,
+    scoring='balanced_accuracy',
+    verbose=3,
+    return_train_score=True,
+    random_state=0
 ).fit(
     Xs_train,
     y_train
@@ -85,5 +123,5 @@ grid = RandomizedSearchCV(
 
 # STORE RESULTS
 
-with open("grid.pickle", "wb") as fh:
+with open(data_dir / "rhs/grid.pickle", "wb") as fh:
     pickle.dump(grid, fh)
