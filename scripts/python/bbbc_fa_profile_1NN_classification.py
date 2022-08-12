04946144d1a5978ebe80259b9d2666f5cfc5971f
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-08-12 16:37:28 +0200

    Optimized memory usage

--- scripts/python/bbbc_fa_profile_1NN_classification.py
+++ scripts/python/bbbc_fa_profile_1NN_classification.py
@@ -2,148 +2,160 @@ import os
 import time
 import pickle
 import sys
-from sklearn.model_selection import LeaveOneGroupOut, cross_validate
+import multiprocessing
+import logging
+from pathlib import Path
+
+from sklearn.model_selection import LeaveOneGroupOut
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import FactorAnalysis
-from sklearn.metrics import (
-    accuracy_score, balanced_accuracy_score, precision_score, f1_score,
-    make_scorer
-)
-import multiprocessing
+
 import pandas
 import numpy
 import pyarrow.parquet as pq
-from pathlib import Path
-import logging
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
-# DATA LOADING
-
-try:
-    paths = snakemake.input.paths
-    moa_path = snakemake.input.moa_path
-    labels_path = snakemake.input.labels
-    output = snakemake.output[0]
-except NameError:
-    data_root = Path("/data/gent/vo/000/gvo00070/vsc42015/datasets/BBBC021")
-    data_dir = data_root / "scip"
-    paths = data_dir.rglob("features.parquet")
-    labels_path = data_dir.parent / "labels.parquet"
-    moa_path = data_root / "BBBC021_v1_moa.csv"
-    output = data_dir / "fa3.pickle"
-
-# paths = list(paths)[:3]
-df = pandas.concat([pq.read_table(p).to_pandas() for p in paths])
-df = df.drop(
-    columns=df.filter(regex='meta_image_.*').columns.tolist() + [
-            "meta_replicate", "meta_imagenumber", "meta_tablenumber"])
-
-df = df[~df.isna().any(axis=1)]
-
-labels = pq.read_table(labels_path).to_pandas()
-df = df.merge(
-    labels,
-    left_on="meta_filename",
-    right_on="meta_image_filename_dapi",
-    suffixes=("_", ""), how="inner"
-)
-
-# Removing interplate variation
-
-qq_dmso = df[df["meta_moa"] == "DMSO"].groupby(
-    "meta_image_metadata_plate_dapi")[df.filter(regex="feat").columns].quantile((0.01, 0.99))
-
-dfs = []
-for idx, gdf in df.groupby("meta_image_metadata_plate_dapi"):
-    df_scaled = (gdf.filter(regex="feat") - qq_dmso.loc[idx, 0.01])
-    df_scaled /= (qq_dmso.loc[idx, 0.99] - qq_dmso.loc[idx, 0.01])
-    df_scaled = pandas.concat([df_scaled, gdf.filter(regex="meta")], axis=1)
-
-    dfs.append(df_scaled)
-
-df = pandas.concat(dfs)
-del dfs
-
-nancols = df.columns[df.isna().any()]
-df = df.drop(columns=nancols)
-
-logging.getLogger().info(str(df.shape))
 
 
 def run_fa(df, moa, n_components):
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        level=logging.INFO,
+        stream=sys.stdout,
+        format="%(process)d;%(asctime)s;%(levelname)s;%(message)s",
+        datefmt='%d-%m-%Y %H:%M:%S'
+    )
+    logger.info("Start %d" % n_components)
 
     start = time.time()
 
     fa = FactorAnalysis(random_state=0, n_components=n_components)
     fa.fit(df[df["meta_compound"] == "DMSO"].filter(regex="feat").sample(n=50000))
+    end_fa = time.time() - start
 
-    dfs = []
-    for idx, gdf in df[df["meta_moa"] != "DMSO"].groupby(["meta_compound", "meta_concentration"]):
-        tmp_df = pandas.Series(data=fa.transform(gdf.filter(regex="feat")).mean(axis=0))
-        tmp_df.index = ["feat_"+str(c) for c in tmp_df.index]
-        tmp_df["meta_compound"] = idx[0]
-        tmp_df["meta_concentration"] = idx[1]
-        dfs.append(tmp_df)
+    grouped = df[df["meta_moa"] != "DMSO"].groupby(["meta_compound", "meta_concentration"])
+    rows = numpy.empty(shape=(grouped.ngroups, n_components), dtype=float)
+    meta = numpy.empty(shape=(grouped.ngroups, 2), dtype=object)
+    for i, (idx, gdf) in enumerate(grouped):
+        rows[i] = fa.transform(gdf.filter(regex="feat")).mean(axis=0)
+        meta[i, 0] = idx[0]
+        meta[i, 1] = idx[1]
 
-    treatment_profiles = pandas.DataFrame(dfs).set_index(["meta_compound", "meta_concentration"])
-    treatment_profiles.index.names = ["compound", "concentration"]
-    del dfs
-
+    treatment_profiles = pandas.DataFrame(
+        data=rows,
+        index=pandas.MultiIndex.from_arrays(meta.T, names=["compound", "concentration"]),
+        columns=["feat_%d" % i for i in range(n_components)],
+    )
     treatment_profiles = treatment_profiles.merge(
         moa, left_index=True, right_index=True).reset_index()
 
-    # Prediction
-
-    results = cross_validate(
-        cv=LeaveOneGroupOut(),
-        X=treatment_profiles.filter(regex="feat"),
-        y=treatment_profiles["moa"],
-        groups=treatment_profiles["compound"],
-        estimator=KNeighborsClassifier(n_neighbors=1, metric="cosine"),
-        return_train_score=True,
-        return_estimator=True,
-        scoring=dict(
-            accuracy=make_scorer(accuracy_score),
-            balanced_accuracy=make_scorer(balanced_accuracy_score),
-            f1_macro=make_scorer(f1_score, average="macro", zero_division=0),
-            precision_macro=make_scorer(precision_score, average="macro", zero_division=0)
-        )
-    )
-
-    preds = []
-    true = []
-    for e, (_, test_index) in zip(
-        results["estimator"],
-        LeaveOneGroupOut().split(
+    n = LeaveOneGroupOut().get_n_splits(
             treatment_profiles.filter(regex="feat"),
             treatment_profiles["moa"],
             treatment_profiles["compound"]
+    )
+    preds = numpy.empty(shape=(n, len(treatment_profiles)), dtype=object)
+    for i, (train_index, test_index) in enumerate(LeaveOneGroupOut().split(
+            treatment_profiles.filter(regex="feat"),
+            treatment_profiles["moa"],
+            treatment_profiles["compound"]
+    )):
+
+        e = KNeighborsClassifier(n_neighbors=1, metric="cosine")
+        e.fit(
+            X=treatment_profiles.filter(regex="feat").iloc[train_index],
+            y=treatment_profiles.iloc[train_index]["moa"]
         )
-    ):
-        pred = e.predict(treatment_profiles.filter(regex="feat").iloc[test_index])
-        preds.extend(pred)
-        true.extend(treatment_profiles["moa"].iloc[test_index])
 
-    del results["estimator"]
+        preds[i, test_index] = e.predict(treatment_profiles.filter(regex="feat").iloc[test_index])
+        preds[i, train_index] = e.predict(treatment_profiles.filter(regex="feat").iloc[train_index])
 
-    logging.getLogger().info("Ready %d" % n_components)
+    end = time.time() - start
+    logger.info("Ready %d in %.2f s (fa: %.2f s)" % (n_components, end, end_fa))
 
-    return n_components, true, preds, results, time.time() - start
+    return n_components, preds, end
 
-N = 20
-components = numpy.arange(start=1, stop=101, step=1)
-moa = pandas.read_csv(moa_path).set_index(["compound", "concentration"])
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        stream=sys.stdout,
+        format="%(process)d;%(asctime)s;%(levelname)s;%(message)s",
+        datefmt='%d-%m-%Y %H:%M:%S'
+    )
 
-def arg_iterator():
-    for _ in range(N):
-        for comp in components:
-            yield (df, moa, comp)
+    logger = logging.getLogger(__name__)
+    logger.info("test")
 
-logging.getLogger().info("Using %s processes" % os.environ["PBS_NUM_PPN"])
-start = time.time()
-with multiprocessing.Pool(processes=int(os.environ["PBS_NUM_PPN"])) as pool:
-    results = pool.starmap(run_fa, iter(arg_iterator()))
-logging.getLogger().info(time.time() - start)
+    multiprocessing.set_start_method("spawn")
 
-with open(output, "wb") as fh:
-    pickle.dump(results, fh)
+    # DATA LOADING
+
+    try:
+        paths = snakemake.input.paths
+        moa_path = snakemake.input.moa_path
+        labels_path = snakemake.input.labels
+        output = snakemake.output[0]
+    except NameError:
+        data_root = Path("/data/gent/vo/000/gvo00070/vsc42015/datasets/BBBC021")
+        data_dir = data_root / "scip"
+        paths = data_dir.rglob("features.parquet")
+        labels_path = data_dir.parent / "labels.parquet"
+        moa_path = data_root / "BBBC021_v1_moa.csv"
+        output = data_dir / "fa3.pickle"
+
+    # paths = list(paths)[:2]
+    df = pandas.concat([pq.read_table(p).to_pandas() for p in paths])
+    logger.info(str(df.shape))
+    df = df.drop(
+        columns=df.filter(regex='meta_image_.*').columns.tolist() + [
+                "meta_replicate", "meta_imagenumber", "meta_tablenumber"])
+    logger.info(str(df.shape))
+
+    df = df[~df.isna().any(axis=1)]
+    logger.info(str(df.shape))
+
+    labels = pq.read_table(labels_path).to_pandas()
+    df = df.merge(
+        labels,
+        left_on="meta_filename",
+        right_on="meta_image_filename_dapi",
+        suffixes=("_", ""), how="inner"
+    )
+    logger.info(str(df.shape))
+
+    # Removing interplate variation
+
+    qq_dmso = df[df["meta_moa"] == "DMSO"].groupby(
+        "meta_image_metadata_plate_dapi")[df.filter(regex="feat").columns].quantile((0.01, 0.99))
+
+    dfs = []
+    for idx, gdf in df.groupby("meta_image_metadata_plate_dapi"):
+        df_scaled = (gdf.filter(regex="feat") - qq_dmso.loc[idx, 0.01])
+        df_scaled /= (qq_dmso.loc[idx, 0.99] - qq_dmso.loc[idx, 0.01])
+        df_scaled = pandas.concat([df_scaled, gdf.filter(regex="meta")], axis=1)
+
+        dfs.append(df_scaled)
+
+    df = pandas.concat(dfs)
+    del dfs
+
+    nancols = df.columns[df.isna().any()]
+    df = df.drop(columns=nancols)
+    logger.info(str(df.shape))
+
+    N = 20
+    components = numpy.arange(start=1, stop=101, step=1)
+    moa = pandas.read_csv(moa_path).set_index(["compound", "concentration"])
+
+    def arg_iterator():
+        for _ in range(N):
+            for comp in components:
+                yield (df, moa, comp)
+
+    logger.info("Using %s processes" % os.environ["PBS_NUM_PPN"])
+    start = time.time()
+    with multiprocessing.Pool(processes=int(os.environ["PBS_NUM_PPN"])) as pool:
+        results = pool.starmap(run_fa, iter(arg_iterator()))
+    logger.info(time.time() - start)
+
+    with open(output, "wb") as fh:
+        pickle.dump(results, fh)
