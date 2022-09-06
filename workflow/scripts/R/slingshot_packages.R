e1c100d416aff98b0b22373bd2a1c91819139b92
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2022-09-06 17:06:33 +0200

    Clean up scripts

--- workflow/scripts/R/slingshot_packages.R
+++
@@ -1,8 +0,0 @@
-# packages
-install.packages("BiocManager")
-install.packages("devtools")
-BiocManager::install("SingleCellExperiment")
-devtools::install_github("wesm/feather/R")
-
-BiocManager::install(version='devel')
-BiocManager::install("slingshot")
